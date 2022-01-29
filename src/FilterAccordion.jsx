5fac4072b9cb3a0c54a9b8ae3a4d1790795d48d6
syfqy <syafiq.ysf@gmail.com>
2022-01-29 18:42:51 +0800

    style: changed ui of filter accordion

--- src/FilterAccordion.jsx
+++ src/FilterAccordion.jsx
@@ -1,52 +1,94 @@
 import React, { useContext } from 'react'
-import Accordion from 'react-bootstrap/Accordion'
-// import Accordion from '@mui/material/Accordion'
 import FilterOption from './FilterOption'
 import { FiltersContext } from './FiltersContext'
 import { camelCase } from 'lodash';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import { styled } from '@mui/material/styles';
 
-// TODO: Replace with api call to generate filters
 import filters from './filters'
+
+// TODO: move to separate components
+const Accordion = styled((props) => (
+    <MuiAccordion disableGutters elevation={0} square {...props} />
+  ))(({ theme }) => ({
+    border: `1px solid ${theme.palette.divider}`,
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+    '&:last-child': {
+      borderRadius: '0px 0px 8px 8px',
+    },
+    '&:before': {
+      display: 'none',
+    },
+    '& .Mui-expanded': {
+        backgroundColor: '#c5dcfa',
+        color: '#003b5c'
+    }
+  }));
+  
+  const AccordionSummary = styled((props) => (
+    <MuiAccordionSummary
+      expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    flexDirection: 'row-reverse',
+    '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+      transform: 'rotate(90deg)',
+    },
+    '& .MuiAccordionSummary-content': {
+      marginLeft: theme.spacing(1),
+      fontSize: '1rem',
+    },
+
+  }));
+  
+  const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+    padding: theme.spacing(2),
+    borderTop: '1px solid rgba(0, 0, 0, .125)',
+    }));
+
 
 export default function FilterAccordion() {
 
     const { dispatch } = useContext(FiltersContext);
 
     return (
-        <>
-            <div className="col-2 filter-container">
-                <div className="accordion">
-                    <div id="filter-main-header">
-                        <h4>Filter by:</h4>
-                        <h4 
-                        onClick={() => dispatch({type: 'CLEAR_FILTERS', filterItem: {}})}
-                        id="clear-filters-btn"
-                        className="text-body text-bold">Clear filters</h4>
-                    </div>
-                    {filters.map((filterItem) => (
-                        <Accordion defaultActiveKey={filterItem.id} 
-                                    key={filterItem.id}
-                                    className="filter-accordion"
-                                    >
-                            <Accordion.Item eventKey={filterItem.id}>
-                                <Accordion.Header className="accordion-header">{filterItem.header}</Accordion.Header>
-                                <Accordion.Body>
 
-                                    {filterItem.options.map((option) => (
-                                        <FilterOption
-                                            key={`${filterItem.id}${option.id}`}
-                                            id={`${filterItem.id}${option.id}`}
-                                            filterHeader={camelCase(filterItem.header)}
-                                            optionName={option.name}
-                                            optionClass={option.class}
-                                        />
-                                    ))}
-                                </Accordion.Body>
-                            </Accordion.Item >
-                        </Accordion>
-                    ))}
-                </div>
+        <div className="col-2 filter-container">
+            <div id="filter-main-header">
+                <h4 id="filter-accordion-main-label">Filter by:</h4>
+                <h4 
+                onClick={() => dispatch({type: 'CLEAR_FILTERS', filterItem: {}})}
+                id="clear-filters-label"
+                className="text-body text-bold">Clear filters</h4>
             </div>
-        </>
-    );
-}
+
+            {filters.map((filterItem) => (
+                         <Accordion key={filterItem.id}>
+
+                             <AccordionSummary>
+                                 {filterItem.header}
+                             </AccordionSummary>
+
+                             <AccordionDetails>
+                                     {filterItem.options.map((option) => (
+                                         <FilterOption
+                                             key={`${filterItem.id}${option.id}`}
+                                             id={`${filterItem.id}${option.id}`}
+                                             filterHeader={camelCase(filterItem.header)}
+                                             optionName={option.name}
+                                             optionClass={option.class} //remove
+                                         />
+                                     ))}
+                            </AccordionDetails>
+                        </Accordion>
+            ))}
+        </div>
+            
+        )
+    };
